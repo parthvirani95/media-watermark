17f6ce7d96403f905362507f42939dc49119598e
Parth Virani <parthvirani95@gmail.com>
2024-10-10 23:35:06 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,6 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MediaWatermark",
-            dependencies: []),
+            dependencies: [],
+            swiftSettings: [
+                            .define("SIMULATOR", .when(platforms: [.iOS], configuration: .debug))
+            ]
+        )
     ]
 )
